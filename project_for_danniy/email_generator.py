b3a299a187c550440a1c8d6fe4ee8fe217c1ec7a
solomka385 <70767805+solomka385@users.noreply.github.com>
2022-12-19 10:51:32 +0300

    Add files via upload

---
+++ project_for_danniy/email_generator.py
@@ -0,0 +1,10 @@
+from transliterate import translit
+from random import sample
+domain_lst = ['@yandex.ru', '@gmail.com', '@mail.ru', '@yahoo.com','@RAMBLER.RU']
+
+def email_gen(fio):
+    fio = fio.replace(' ', '')
+    fio_en = translit(fio, 'ru', reversed=True)
+    email = fio_en + ''.join(sample(domain_lst, 1))
+    return email.upper()
+
